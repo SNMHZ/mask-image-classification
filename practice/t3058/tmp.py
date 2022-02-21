f40e6af084a0701e519bc4c1e3be470c86ac67ba
jinsu <wlstnwkwjsrj@naver.com>
2022-02-21 09:24:18 +0000

    1

---
+++ practice/t3058/tmp.py
@@ -0,0 +1,93 @@
+import os
+import pandas as pd
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from torchvision import transforms
+from torchvision.transforms import Resize, ToTensor, Normalize
+
+
+# 테스트 데이터셋 폴더 경로를 지정해주세요.
+test_dir = '/opt/ml/input/data/eval'
+
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000):
+        super(MyModel, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(64, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+class TestDataset(Dataset):
+    def __init__(self, img_paths, transform):
+        self.img_paths = img_paths
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image = Image.open(self.img_paths[index])
+
+        if self.transform:
+            image = self.transform(image)
+        return image
+
+    def __len__(self):
+        return len(self.img_paths)
+
+
+# meta 데이터와 이미지 경로를 불러옵니다.
+submission = pd.read_csv(os.path.join(test_dir, 'info.csv'))
+image_dir = os.path.join(test_dir, 'images')
+
+# Test Dataset 클래스 객체를 생성하고 DataLoader를 만듭니다.
+image_paths = [os.path.join(image_dir, img_id) for img_id in submission.ImageID]
+transform = transforms.Compose([
+    Resize((512, 384), Image.BILINEAR),
+    ToTensor(),
+    Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
+])
+dataset = TestDataset(image_paths, transform)
+
+loader = DataLoader(
+    dataset,
+    shuffle=False
+)
+
+# 모델을 정의합니다. (학습한 모델이 있다면 torch.load로 모델을 불러주세요!)
+device = torch.device('cuda')
+model = MyModel(num_classes=18).to(device)
+model.eval()
+
+# 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
+all_predictions = []
+for images in loader:
+    with torch.no_grad():
+        images = images.to(device)
+        pred = model(images)
+        pred = pred.argmax(dim=-1)
+        all_predictions.extend(pred.cpu().numpy())
+submission['ans'] = all_predictions
+
+# 제출할 파일을 저장합니다.
+submission.to_csv(os.path.join(test_dir, 'submission.csv'), index=False)
+print('test inference is done!')
+
+
