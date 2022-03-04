4d087acf4c3876ff405e61646274a2e9011583ed
SNMHZ <snmhz325@gmail.com>
2022-03-04 09:00:16 +0000

    feat: Add final codes

--- practice/t3146/train.py
+++ practice/t3146/train.py
@@ -1,52 +1,291 @@
+import argparse
+import glob
+import json
+import multiprocessing
+import os
+import random
+import re
+from importlib import import_module
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
-import torch.nn as nn
-from tqdm import tqdm
-
+from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
-from torch.optim import Optimizer
+from torch.utils.tensorboard import SummaryWriter
 
-def train_one_epoch(model: nn.Module, optimizer: Optimizer, data_loader: DataLoader, device, epoch):
-    model.train()
-    model.zero_grad()
-    tqdm_dataloader = tqdm(data_loader)
-    total_batch = 0
-    for targets in tqdm_dataloader:
-        images = targets['image'].to(device)
-        labels = targets['label'].to(device)
-        total_batch += len(images)
+from dataset import MaskBaseDataset
+from loss import create_criterion
 
-        logits = model(images)
-        loss = nn.CrossEntropyLoss()(logits, labels)
-        
-        # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+from dataset import BaseAugmentation, CustomAugmentation, CustomAugmentation_OnlyCrop, CustomAugmentation_RotateLeft_Crop, CustomAugmentation_RotateRight_Crop, CustomAugmentation_RotateRight_Crop, CustomAugmentation_Flip
+from torchmetrics import F1Score
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
 
-        tqdm_dataloader.set_description(
-            f"Epoch {epoch + 1}, lr is {optimizer.param_groups[0]['lr']:.6f} loss {loss.item():.3f}")
 
-# Mission
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
-# 1. 우리가 중간중간 모델의 결과를 저장하고 싶은 경우가 있습니다. 
-# 학습되는 중간에 좋은 성능의 모델이 발견이 되었으면 모델을 저장하는 것이 좋은데요. 
-# 이러한 체크포인트 저장 과정을 Evaluation에 추가해서 체크포인트 모델이 저장되도록 설계하세요.
 
-# 2. 모델 평가에 Metric을 올바르게 사용하는 것도 중요합니다. 
-# Evaluation 과정에서 모델 학습 결과를 Loss와 Accuracy만 보는것 보다는 다른 Metric도 참고하는 것도 좋습니다. 
-# F1-score Metric 을 구현 or 코드를 찾아서 추가하고, 매 에폭마다 결과를 확인할 수 있도록 설계하세요.
+def grid_image(np_images, gts, preds, n=16, shuffle=False):
+    batch_size = np_images.shape[0]
+    assert n <= batch_size
 
-# 3. Training Process에 어느정도 익숙해지셨다면 Gradient Accumulation을 내 프로젝트에 적용시켜보세요. 
-# 적용한다고 반드시 성능이 올라가는 것은 아닐겁니다. 
-# 적용을 했다면 전후 성능 비교를 해봐야겠지요?
-# (코드는 강의자료에 있습니다.)
+    choices = random.choices(range(batch_size), k=n) if shuffle else list(range(n))
+    figure = plt.figure(figsize=(12, 18 + 2))  # cautions: hardcoded, 이미지 크기에 따라 figsize 를 조정해야 할 수 있습니다. T.T
+    plt.subplots_adjust(top=0.8)  # cautions: hardcoded, 이미지 크기에 따라 top 를 조정해야 할 수 있습니다. T.T
+    n_grid = int(np.ceil(n ** 0.5))
+    tasks = ["mask", "gender", "age"]
+    for idx, choice in enumerate(choices):
+        gt = gts[choice].item()
+        pred = preds[choice].item()
+        image = np_images[choice]
+        gt_decoded_labels = MaskBaseDataset.decode_multi_class(gt)
+        pred_decoded_labels = MaskBaseDataset.decode_multi_class(pred)
+        title = "\n".join([
+            f"{task} - gt: {gt_label}, pred: {pred_label}"
+            for gt_label, pred_label, task
+            in zip(gt_decoded_labels, pred_decoded_labels, tasks)
+        ])
 
-# 4. Optimizer의 스케줄러가 종류에 따라 어떻게 Learning rate를 변경해 가는지, 옵션에 따라 또 어떻게 스케쥴링하게 되는지 확인해보세요. 
-# 대표적으로 많이 사용하는 SGD 와 Adam Optimizer를 각각 구현해 보고 비교해 봅시다. 
-# 아래 Further Reading에 좋은 예제가 있습니다. 
+        plt.subplot(n_grid, n_grid, idx + 1, title=title)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(image, cmap=plt.cm.binary)
 
-# 5. 새로운 Loss를 만들어 봅시다. 
-# 새로운 Loss를 적용해서 학습에 사용해보겠습니다. 
-# (Label smoothing, Focal Loss, F1 Loss 등) 무엇이든지 상관없습니다. 
-# 구현 후 학습에 활용해서 비교해보세요!
+    return figure
+
+
+def increment_path(path, exist_ok=False):
+    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+
+def train(data_dir, model_dir, args):
+    seed_everything(args.seed)
+
+    save_dir = increment_path(os.path.join(model_dir, args.name))
+
+    # -- settings
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # -- dataset
+    dataset_module = getattr(import_module("dataset"), args.dataset)  # default: MaskBaseDataset
+    dataset = dataset_module(
+        data_dir=data_dir,
+    )
+    num_classes = dataset.num_classes  # 18
+
+    # -- augmentation
+    transform_module = getattr(import_module("dataset"), args.augmentation)  # default: BaseAugmentation
+    transform = [BaseAugmentation(
+                    resize=args.resize,
+                    mean=dataset.mean,
+                    std=dataset.std,
+                    ),
+                    ]
+    dataset.set_transform(transform)
+
+    # -- data_loader
+    train_set, val_set = dataset.split_dataset()
+
+    train_loader = DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        num_workers=multiprocessing.cpu_count() // 2,
+        shuffle=True,
+        pin_memory=use_cuda,
+        drop_last=True,
+    )
+
+    val_loader = DataLoader(
+        val_set,
+        batch_size=args.valid_batch_size,
+        num_workers=multiprocessing.cpu_count() // 2,
+        shuffle=False,
+        pin_memory=use_cuda,
+        drop_last=True,
+    )
+
+    # -- model
+    model_module = getattr(import_module("model"), args.model)  # default: BaseModel
+    model = model_module(
+        num_classes=num_classes
+    ).to(device)
+    model = torch.nn.DataParallel(model)
+
+    # -- loss & metric
+    criterion = create_criterion(args.criterion)  # default: cross_entropy
+    opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: SGD
+    optimizer = opt_module(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=args.lr,
+        weight_decay=5e-4
+    )
+    scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+    f1 = F1Score(num_classes=18).to(device)
+    # -- logging
+    logger = SummaryWriter(log_dir=save_dir)
+    with open(os.path.join(save_dir, 'config.json'), 'w', encoding='utf-8') as f:
+        json.dump(vars(args), f, ensure_ascii=False, indent=4)
+
+    best_val_acc = 0
+    best_val_loss = np.inf
+    for epoch in range(args.epochs):
+        # train loop
+        model.train()
+        loss_value = 0
+        matches = 0
+        f1_score = 0
+        figure = None
+        for idx, train_batch in enumerate(train_loader):
+            inputs, labels = train_batch
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+
+            outs: torch.Tensor = model(inputs)
+            # preds = torch.argmax(outs, dim=-1)
+            mask_pred, age_pred, gender_pred = torch.stack([torch.argmax(_pred, dim=-1) for _pred in outs.split(3, dim=1)])
+            preds = MaskBaseDataset.encode_multi_class(mask_pred, gender_pred, age_pred)
+            loss = criterion(outs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            loss_value += loss.item()
+            matches += (preds == labels).sum().item()
+            f1_score += f1(preds, labels).item()
+            if (idx + 1) % args.log_interval == 0:
+                train_loss = loss_value / args.log_interval
+                train_acc = matches / args.batch_size / args.log_interval
+                train_f1 = f1_score / (idx+1)
+                
+                inputs_np = torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
+                inputs_np = dataset_module.denormalize_image(inputs_np, dataset.mean, dataset.std)
+                figure = grid_image(
+                    inputs_np, labels, preds, n=16, shuffle=args.dataset != "MaskSplitByProfileDataset"
+                )
+                    
+                current_lr = get_lr(optimizer)
+                print(
+                    f"Epoch[{epoch}/{args.epochs}]({idx + 1}/{len(train_loader)}) || "
+                    f"training loss {train_loss:4.4} || training accuracy {train_acc:4.2%} || "
+                    f"training f1 {train_f1:4.4} || "
+                    f"lr {current_lr}"
+                )
+                logger.add_scalar("Train/loss", train_loss, epoch * len(train_loader) + idx)
+                logger.add_scalar("Train/accuracy", train_acc, epoch * len(train_loader) + idx)
+                logger.add_figure("feeding img", figure, epoch)
+
+                loss_value = 0
+                matches = 0
+
+        scheduler.step()
+
+        # val loop
+        with torch.no_grad():
+            print("Calculating validation results...")
+            model.eval()
+            val_loss_items = []
+            val_acc_items = []
+            figure = None
+            f1_score = 0
+            for val_batch in val_loader:
+                inputs, labels = val_batch
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                outs: torch.Tensor = model(inputs)
+                # preds = torch.argmax(outs, dim=-1)
+                mask_pred, age_pred, gender_pred = torch.stack([torch.argmax(_pred, dim=-1) for _pred in outs.split(3, dim=1)])
+                preds = MaskBaseDataset.encode_multi_class(mask_pred, gender_pred, age_pred).to(device)
+
+                loss_item = criterion(outs, labels).item()
+                acc_item = (labels == preds).sum().item()
+                val_loss_items.append(loss_item)
+                val_acc_items.append(acc_item)
+                f1_score += f1(preds, labels)
+                if figure is None:
+                    inputs_np = torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
+                    inputs_np = dataset_module.denormalize_image(inputs_np, dataset.mean, dataset.std)
+                    figure = grid_image(
+                        inputs_np, labels, preds, n=16, shuffle=args.dataset != "MaskSplitByProfileDataset"
+                    )
+            f1_score /= len(val_loader)
+            val_loss = np.sum(val_loss_items) / len(val_loader)
+            val_acc = np.sum(val_acc_items) / len(val_set)
+            best_val_loss = min(best_val_loss, val_loss)
+            if val_acc > best_val_acc:
+                print(f"New best model for val accuracy : {val_acc:4.2%}! saving the best model..")
+                torch.save(model.module.state_dict(), f"{save_dir}/best.pth")
+                best_val_acc = val_acc
+            torch.save(model.module.state_dict(), f"{save_dir}/last.pth")
+            print(
+                f"[Val] acc : {val_acc:4.2%}, loss: {val_loss:4.2} || "
+                f"best acc : {best_val_acc:4.2%}, best loss: {best_val_loss:4.2} || "
+                f"f1 score : {f1_score:4.2}"
+            )
+            logger.add_scalar("Val/loss", val_loss, epoch)
+            logger.add_scalar("Val/accuracy", val_acc, epoch)
+            logger.add_figure("results", figure, epoch)
+            print()
+
+
+if __name__ == '__main__':
+    torch.cuda.empty_cache()
+    parser = argparse.ArgumentParser()
+
+    # Data and model checkpoints directories
+    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
+    parser.add_argument('--epochs', type=int, default=1, help='number of epochs to train (default: 1)')
+    parser.add_argument('--dataset', type=str, default='MaskBaseDataset', help='dataset augmentation type (default: MaskBaseDataset)')
+    parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
+    parser.add_argument("--resize", nargs="+", type=int, default=[128, 96], help='resize size for image when training')
+    parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
+    parser.add_argument('--valid_batch_size', type=int, default=64, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--model', type=str, default='ModFcModel_NonFreeze_Res18', help='model type (default: BaseModel)')
+    parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer type (default: SGD)')
+    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
+    parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
+    parser.add_argument('--criterion', type=str, default='cross_entropy', help='criterion type (default: cross_entropy)')
+    parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
+    parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
+    parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './model'))
+
+    args = parser.parse_args()
+    print(args)
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+
+    train(data_dir, model_dir, args)
